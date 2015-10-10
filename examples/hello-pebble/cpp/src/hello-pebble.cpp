5d2efd0697b281e5b47f778ddd9a11b813d4f2e7
Ed Ball <ejball@gmail.com>
2015-10-10 12:32:29 -0700

    hello-pebble.cpp missed a file

--- examples/hello-pebble/cpp/src/hello-pebble.cpp
+++ examples/hello-pebble/cpp/src/hello-pebble.cpp
@@ -5,6 +5,7 @@ using namespace PbCpp;
 int main() {
   PbWindow window;
   PbTextLayer textLayer;
+  textLayer.create(PbRect(0, 0, 144, 40)).text("Hello, Pebble!");
   window.create().addChild(textLayer).show();
   app_event_loop();
   return 0;
